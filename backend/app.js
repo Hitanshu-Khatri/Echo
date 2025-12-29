60e850deb0d42b965c37af1caf677d5210e47e9e
Hitanshu-Khatri <hitanshuskhatri@gmail.com>
2025-12-29 15:57:58 +0530

    Fixed IP issue

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
-app.set("port","0.0.0.0",(process.env.PORT || 8000));
+app.set("port",(process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb",extended: true}));
@@ -23,7 +23,7 @@ const start = async ()=>{
     app.set("mongo_user")
     const connectionDb = await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDb");
-    server.listen(app.get("port"),()=>{
+    server.listen(app.get("port"),"0.0.0.0",()=>{
         console.log(`Server is running on port ${app.get("port")}`);
     });
 }

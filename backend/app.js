e64507d957d62b79f7b1c7dfc88734b29d041733
Hitanshu-Khatri <hitanshuskhatri@gmail.com>
2025-12-29 15:41:24 +0530

    Fix backend to listen on 0.0.0.0 for Docker networking

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
-app.set("port",(process.env.PORT || 8000));
+app.set("port","0.0.0.0",(process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb",extended: true}));
